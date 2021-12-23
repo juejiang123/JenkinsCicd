0d8ef603e99a6164c5b40d075b314bac8495e4c1
霍振鹏 <huozhenpengdyx@gmail.com>
2021-12-23 15:13:30 +0800

    二次提交，测试集成环境

--- src/main/java/com/jenkins/demo1.java
+++ src/main/java/com/jenkins/demo1.java
@@ -3,5 +3,6 @@ package com.jenkins;
 public class demo1 {
     public static void main(String[] args) {
         System.out.println("First submit");
+        System.out.println("twice submit");
     }
 }
