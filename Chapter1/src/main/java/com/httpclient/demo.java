384758bf6fae28a3b7aa629bae72b0cd002de8e7
霍振鹏 <huozhenpengdyx@gmail.com>
2021-12-23 21:57:32 +0800

    第9次提交，修改httpclient过期的client类

--- Chapter1/src/main/java/com/httpclient/demo.java
+++ Chapter1/src/main/java/com/httpclient/demo.java
@@ -3,7 +3,9 @@ package com.httpclient;
 
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
 import org.testng.annotations.Test;
 
@@ -15,7 +17,7 @@ public class demo {
     public void getTest(){
         String result;
         HttpGet get = new HttpGet("http://www.baidu.com");
-        DefaultHttpClient client = new DefaultHttpClient();
+        CloseableHttpClient client = HttpClientBuilder.create().build();
         try {
             CloseableHttpResponse response = client.execute(get);
             result= EntityUtils.toString(response.getEntity());
