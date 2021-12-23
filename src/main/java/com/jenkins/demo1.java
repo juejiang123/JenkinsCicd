fc7def50b444ac1a67220dc86ced970a53dfccdb
霍振鹏 <huozhenpengdyx@gmail.com>
2021-12-23 16:08:12 +0800

    第六次提交，测试集成环境

--- src/main/java/com/jenkins/demo1.java
+++ src/main/java/com/jenkins/demo1.java
@@ -7,5 +7,6 @@ public class demo1 {
         System.out.println("third submit");
         System.out.println("1111");
         System.out.println("2222");
+        System.out.println("33333");
     }
 }
