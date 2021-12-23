fedc78ea9d91fe71ac4078ccfbaf8dd17ea04570
霍振鹏 <huozhenpengdyx@gmail.com>
2021-12-23 22:18:29 +0800

    第10次提交，增加testng依赖

--- Chapter1/src/main/java/com/httpclient/demo.java
+++ Chapter1/src/main/java/com/httpclient/demo.java
@@ -4,7 +4,6 @@ package com.httpclient;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
 import org.testng.annotations.Test;
