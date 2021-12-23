198cd9cac01dcc47193a6d6eb7b569bf5df5fbf2
霍振鹏 <huozhenpengdyx@gmail.com>
2021-12-23 15:20:26 +0800

    三次提交，测试集成环境

--- src/main/java/com/jenkins/demo1.java
+++ src/main/java/com/jenkins/demo1.java
@@ -4,5 +4,6 @@ public class demo1 {
     public static void main(String[] args) {
         System.out.println("First submit");
         System.out.println("twice submit");
+        System.out.println("third submit");
     }
 }
